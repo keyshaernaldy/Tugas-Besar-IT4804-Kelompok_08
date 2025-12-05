e4a6de6aeab830bb5859b3e22764f037619401e2
Farida Syafa Kusuma Widi <farida.syafa.kw@gmail.com>
2025-12-05 07:01:22 +0700

    Remove unused function declarations from main.h
    Removed function declarations from main.h.

--- TUBES_STRUKDAT/main.h
+++ TUBES_STRUKDAT/main.h
@@ -20,35 +20,3 @@ struct Relation{
     string namaProject;
     Relation *next;
 };
-
-void createListDeveloper(Developer *&first);
-
-void createListClient(Client *&first);
-
-void createListRelation(Relation *&first);
-
-void createNewDeveloper(string nama, Developer *&newDeveloper);
-
-void createNewClient(string nama, Client *&newClient);
-
-void createNewRelation(Developer *dev, Client *cli, string namaProject, Relation *&newRelation);
-
-void insertFirstDeveloper(Developer *&first, Developer *newDeveloper);
-
-void insertFirstClient(Client *&first, Client *newClient);
-
-void insertFirstRelation(Relation *&first, Relation *newRelation);
-
-void deleteFirstDeveloper(Developer *&first, Developer *&deletedDeveloper);
-
-void deleteFirstClient(Client *&first, Client *&deletedClient);
-
-void deleteFirstRelation(Relation *&first, Relation *&deletedRelation);
-
-void printAllDeveloper(Developer *first);
-
-void printAllClient(Client *first);
-
-void printAllRelation(Relation *first);
-
-#endif // MAIN_H_INCLUDED
